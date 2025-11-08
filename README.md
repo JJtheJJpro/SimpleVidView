a2625f0ccaccaf27c0b97fd5bb081adcfe6f3c3f
JJtheJJpro <jjthejjpro@gmail.com>
2025-11-08 12:56:09 -0700

    restarting...

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Vid View
+
+A very simple video/gif viewer, using ffmpeg api, and simple options.  Play/Pause, searchable bar, frame-by-frame keystrokes, etc.
--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=delayimp");
-
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("logo.ico");
-    res.compile().unwrap();
-}
